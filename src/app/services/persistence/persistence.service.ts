303bcfad805be28aaee209758294632bb359228b
littleant111 <wangtao_2016@126.com>
2019-07-22 18:02:44 +0800

    添加缓存persistence服务,debug信息服务和平台plaform服务;

---
+++ src/app/services/persistence/persistence.service.ts
@@ -0,0 +1,80 @@
+import { Injectable } from '@angular/core';
+import { File } from '@ionic-native/file/ngx';
+import * as _ from 'lodash';
+
+import { LoggerService } from '../logger/logger.service';
+import { PlatformService } from '../platform/platform.service';
+
+import { FileStorage } from './storage/file-storage';
+import { LocalStorage } from './storage/local-storage';
+
+const Keys = {
+  APP_IDENTITY: network => 'appIdentity-' + network,
+  CONFIG: 'config',
+  PROFILE: 'profile',
+};
+
+interface Storage {
+  get(k: string): Promise<any>;
+  set(k: string, v): Promise<void>;
+  remove(k: string): Promise<void>;
+  create(k: string, v): Promise<void>;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersistenceService {
+  public storage: Storage;
+  constructor(
+    private logger: LoggerService,
+    private platform: PlatformService,
+    private file: File
+  ) { 
+    this.logger.info('Persistence service initialized')
+  };
+
+  public load() {
+    this.storage = this.platform.isCordova ? new FileStorage(this.file, this.logger) : new LocalStorage(this.logger);
+  }
+
+  setAppIdentity(network: string, data) {
+    return this.storage.set(Keys.APP_IDENTITY(network), data);
+  }
+
+  getAppIdentity(network: string) {
+    return this.storage.get(Keys.APP_IDENTITY(network));
+  }
+
+  removeAppIdentity(network: string) {
+    return this.storage.remove(Keys.APP_IDENTITY(network));
+  }
+
+  getConfig() {
+    return this.storage.get(Keys.CONFIG);
+  }
+
+  storeConfig(config: object) {
+    return this.storage.set(Keys.CONFIG, config);
+  }
+
+  clearConfig() {
+    return this.storage.remove(Keys.CONFIG);
+  }
+
+  storeProfile(profile): Promise<void> {
+    return this.storage.set(Keys.PROFILE, profile);
+  }
+
+  getProfile(): Promise<any> {
+    return new Promise(resolve => {
+      this.storage.get(Keys.PROFILE).then(profile => {
+        resolve(profile);
+      });
+    });
+  }
+
+  deleteProfile() {
+    return this.storage.remove(Keys.PROFILE);
+  }
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { LoggerService } from '../services/logger/logger.service'
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +9,16 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private logger: LoggerService) {
 
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.testLogger()
+  }
+  testLogger() {
+    this.logger.debug('test logger: success!');
+  }
 }
