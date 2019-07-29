7ef5297a702607eac3c94ba823c4bbb2a67a64d6
littleant111 <wangtao_2016@126.com>
2019-07-29 16:31:13 +0800

    [add] 添加copy direcitive

--- src/app/directives/copy-to-clipboard/copy-to-clipboard.directive.ts
+++ src/app/directives/copy-to-clipboard/copy-to-clipboard.directive.ts
@@ -1,10 +1,50 @@
-import { Directive } from '@angular/core';
+import { Directive, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
+import { Clipboard } from '@ionic-native/clipboard/ngx'
+import { PlatformService } from 'src/app/services/platform/platform.service';
+import { LoggerService } from 'src/app/services/logger/logger.service';
+import { TranslateService } from '@ngx-translate/core';
+import { CopyService } from 'src/app/services/copy/copy.service';
 
 @Directive({
-  selector: '[appCopyToClipboard]'
+  selector: '[appCopyToClipboard]',
+  inputs: ['value: appCopyToClipboard'],
+  host: {
+    '(click)': 'copy()'
+  }
 })
 export class CopyToClipboardDirective {
+  public value: string;
+  // public hideToast: boolean;
+  private dom: Document;
 
-  constructor() { }
+  constructor(
+    @Inject(DOCUMENT) dom: Document,
+    public clipboard: Clipboard,
+    public platform: PlatformService,
+    public logger: LoggerService,
+    public translate: TranslateService,
+    private copyService: CopyService
+  ) { 
+    this.dom = dom;
+  }
 
+  private copyBrowser() {
+    let textarea = this.dom.createElement('textarea');
+    this.dom.body.appendChild(textarea);
+    textarea.value = this.value;
+    textarea.select();
+    this.dom.execCommand('copy');
+    this.dom.body.removeChild(textarea);
+  }
+
+  public copy() {
+    if(!this.value) return;
+    try {
+      this.copyService.copy(this.value);
+    } catch(err) {
+      if(err) this.logger.warn(err.message);
+      this.copyBrowser();
+    }
+  }
 }
